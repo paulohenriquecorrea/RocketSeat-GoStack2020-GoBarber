643e472c2c002f1d6a636ae5a3197682979275fc
unknown <ph.sistemas.ti@gmail.com>
2023-12-06 15:05:12 -0400

    Finalizado etapa validar data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,11 +1,41 @@
-import { request, response, Router } from 'express';
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
-// POST http://localhost:3333/appointments
-appointmentsRouter.post('/', (request, response) => {
-  return response.json({ Message: 'Hello World' });
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request: any, response: any) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find((appointment) => {
+    return isEqual(parsedDate, appointment.date);
+  });
+  console.log(`Retorno do findAppointment: ${findAppointmentInSameDate}`);
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
